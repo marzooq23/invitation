88b903db6c2ec1706b381b9678cb87b4fd78a867
marzooq23 <mohamedmarzooq23@gmail.com>
2026-09-18 12:55:48 +0000

    Point site config at marzooqwedsfarwizah org

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://marzooq23.github.io',
-  base: '/invitation/',
+  site: 'https://marzooqwedsfarwizah.github.io',
+  base: '/',
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
