a255a4c9d6515a745736f1a4b4b4f4aca109a0d3
marzooq23 <mohamedmarzooq23@gmail.com>
2026-09-18 12:46:44 +0000

    Deploy to GitHub Pages with Astro build workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://marzooq23.github.io',
+  base: '/invitation/',
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
